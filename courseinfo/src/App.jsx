7beb2631e696381bec65d7cd21bb23fe4bf445ae
Marwan Tawfik <marwantawfik2048@gmail.com>
2025-08-27 01:34:19 +0300

    1.2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,24 +4,21 @@ const Header = () => {
 }
 
 const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
   return (
     <div>
-      <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-      </p>
+      <Part name='Fundamentals of React' exercises={10} />
+      <Part name='Using props to pass data' exercises={7} />
+      <Part name='State of a component' exercises={14} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+      <p>
+        {props.name} {props.exercises}
+      </p>
   )
 }
 
